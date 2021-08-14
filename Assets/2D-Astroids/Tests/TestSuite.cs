50299964ff1ae333e588b9452cfedf607814e229
Kieran <kieranc.inbox@gmail.com>
2021-08-15 05:45:26 +1000

    Pushing 2 working tests

--- Assets/2D-Astroids/Tests/TestSuite.cs
+++ Assets/2D-Astroids/Tests/TestSuite.cs
@@ -5,25 +5,51 @@ using UnityEngine.TestTools;
 
 public class TestSuite
 {
-	private GameManager gameManager;
+	private GameManager game;
 
-// 1
 	[UnityTest]
 	public IEnumerator AsteroidsMovesAfterSpawned()
 	{
-		// 2
+		// Makes the game as a GameObject = gameGameObject.
 		GameObject gameGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-		gameManager = gameGameObject.GetComponent<GameManager>();
+		// Gets the GameManager, which is on the game.
+		game = gameGameObject.GetComponent<GameManager>();
+		// Gets the AsteroidSpawner, which is a GameObject in the children of the gameGameObject. 
 		AsteroidSpawner asteroidSpawner = gameGameObject.GetComponentInChildren<AsteroidSpawner>();
-		// 3
+		// Spawns one astroid as asteroid.
 		GameObject asteroid = asteroidSpawner.SpawnOneAsteroid();
-		// 4
+		// Gets its initial Position as a Vector 2.
 		Vector2 initialPos = asteroid.transform.position;
-		// 5
+		// Waits one second, because all tests are a coroutine you need to have one return.
 		yield return new WaitForSeconds(0.1f);
-		// 6
+		// Checks if the new position is different to the old and if so it passes the test.
 		Assert.AreNotEqual(asteroid.transform.position, initialPos);
-		// 7
-		Object.Destroy(gameManager.gameObject);
+		// Destroy the game after the test so nothing is left in the scene.
+		Object.Destroy(game.gameObject);
+	}
+	
+	[UnityTest]
+	public IEnumerator GameOverOccursOnAsteroidCollision()
+	{
+		// Makes the game as a GameObject = gameGameObject.
+		GameObject gameGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+		// Gets the GameManager, which is on the game.
+		game = gameGameObject.GetComponent<GameManager>();
+		// Get the current amount of lives.
+		int initialLives = game.lives;
+		// Gets the AsteroidSpawner, which is a GameObject in the children of the gameGameObject. 
+		AsteroidSpawner asteroidSpawner = gameGameObject.GetComponentInChildren<AsteroidSpawner>();
+		// Gets the AsteroidSpawner, which is a GameObject in the children of the gameGameObject. 
+		Player player = gameGameObject.GetComponentInChildren<Player>();
+		// Spawns one astroid as asteroid.
+		GameObject asteroid = asteroidSpawner.SpawnOneAsteroid();
+		// Puts the asteroid ontop of the player.
+		asteroid.transform.position = player.gameObject.transform.position;
+		// Waits 0.1 Seconds, as yes this is a coroutine.
+		yield return new WaitForSeconds(0.1f);
+		// Checks if the player has lost lives.
+		Assert.Less(game.lives, initialLives);
+		// Tears down the scene so there is nothing left between tests.
+		Object.Destroy(game.gameObject);
 	}
 }
